10d3acf102f2aa793127ef1182e22262706917e2
kaiss elghoul <kaiss.ghoul@esprit.tn>
2024-02-22 11:29:18 +0100

    Premier commit du frontend

---
+++ src/app/c3/c3.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-joinnow',
+  templateUrl: './c3.component.html',
+  styleUrls: ['./c3.component.css'],
+})
+export class C3Component implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
